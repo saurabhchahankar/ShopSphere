5a7a5cc528168a4785180a73627524f0353f34c3
Saurabh Chahankar <saurabhchahankar@gmail.com>
2026-09-23 16:03:27 +0530

    Set up ShopSphere frontend foundation

---
+++ client/src/pages/home.jsx
@@ -0,0 +1,12 @@
+const Home = () => {
+  return (
+    <>
+      <div>
+        <h1>Welcome to ShopSphere</h1>
+        <p>Your online shopping destination.</p>
+      </div>
+    </>
+  )
+}
+
+export default Home;
